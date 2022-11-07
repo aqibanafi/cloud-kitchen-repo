32069ed21416590f2ae51a4eae532345facd7354
unknown <you@example.com>
2022-11-08 03:12:03 +0600

    Limited Data Load For Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Banner from './Banner';
+import LimitedServices from './LimitedServices';
 
 const Home = () => {
+    //Load Data From Loader
+    const services = useLoaderData()
     return (
         <div>
-           <Banner></Banner>
+            <div>
+                <Banner></Banner>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <LimitedServices key={service._id} service={service}></LimitedServices>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Home/LimitedServices.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const LimitedServices = ({ service }) => {
+    //Distructure Data 
+    const { _id, title, image, description, rating, price } = service;
+    return (
+        <div className='flex flex-col items-center p-10 shadow-xl rounded-xl'>
+            <div className='mb-10'>
+                <img className='h-64 w-96 rounded-lg hover:scale-110 duration-300 ease-out' src={image} alt="" />
+            </div>
+            <div>
+                <h2 className='text-3xl font-bold mb-5'>{title}</h2>
+                <p className='mb-5'>{description.length > 100 ? description.slice(0, 100) + '...' : description.length}</p>
+                <div className='flex justify-between'>
+                    <p className='font-bold'>Price: $ {price} <small>/person</small></p>
+                    <div className='flex items-center gap-5'>
+                        <p>Ratings: {rating}</p>
+                        <FaStar className='text-yellow-500'></FaStar>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-10'>
+                <Link to={`/servicedetail/${_id}`}>
+                    <button className="btn btn-success">Success</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default LimitedServices;
